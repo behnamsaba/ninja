4afd3eeeef007c43157409948c4d19f62a078efd
ben <behnams71@gmail.com>
2022-11-12 20:10:13 -0800

    ninja

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+debug = DebugToolbarExtension(app)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/form")
+def ui_form():
+    prompts = story.prompts
+    return render_template("form.html",prompts=prompts)
+
+
+@app.route("/story")
+def show_story():
+    text = story.generate(request.args)
+
+    return render_template("story.html", text=text)
